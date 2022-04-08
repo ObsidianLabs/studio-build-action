55767649b6f7bbc5ffad80bd807dae4e42e1684a
北屿一木 <20736452+iNorthIsle@users.noreply.github.com>
2022-04-08 16:34:25 +0800

    chore: add try catch

--- index.js
+++ index.js
@@ -68,47 +68,51 @@ const setEnvVariable = (name, value) => {
  * Installs NPM dependencies and builds/releases the Electron app
  */
 const runAction = () => {
-  const workingDirectory = getEnvVariable('working-directory')
-  if (workingDirectory) {
-    chdir(workingDirectory)
+  try {
+    const workingDirectory = getEnvVariable('working-directory')
+    if (workingDirectory) {
+      chdir(workingDirectory)
+    }
+    const platform = getPlatform();
+
+    // Make sure `package.json` file exists
+    verifyPackageJson();
+
+    // Copy 'github_token' input variable to 'GH_TOKEN' env variable (required by `electron-builder`)
+    setEnvVariable('GH_TOKEN', getEnvVariable('github_token', true));
+
+    // Require code signing certificate and password if building for macOS. Export them to environment
+    // variables (required by `electron-builder`)
+    if (platform === 'mac') {
+      setEnvVariable('CSC_LINK', getEnvVariable('mac_certs'));
+      setEnvVariable('CSC_KEY_PASSWORD', getEnvVariable('mac_certs_password'));
+    } else if (platform === 'windows') {
+      setEnvVariable('CSC_LINK', getEnvVariable('windows_certs'));
+      setEnvVariable('CSC_KEY_PASSWORD', getEnvVariable('windows_certs_password'));
+    }
+
+    setEnvVariable('BUILD', getEnvVariable('build'))
+    setEnvVariable('PROJECT', getEnvVariable('project'))
+    setEnvVariable('PROJECT_NAME', getEnvVariable('project_name'))
+    setEnvVariable('CI', getEnvVariable('ci'))
+    setEnvVariable('GENERATE_SOURCEMAP', getEnvVariable('generate_sourcemap') || false)
+    setEnvVariable('ENABLE_AUTH', getEnvVariable('enable_auth') || false)
+    setEnvVariable('REACT_APP_MIXPANEL_TOKEN', getEnvVariable('react_app_mixpanel_token'))
+    setEnvVariable('PREMIUM_EDITOR', getEnvVariable('premium_editor'))
+    setEnvVariable('BUILD_ID', getEnvVariable('build_id'))
+    setEnvVariable('COMMIT_ID', getEnvVariable('commit_id').toString())
+    setEnvVariable('BUILD_TIME', getEnvVariable('build_time') ? new Date(Number(getEnvVariable('build_time'))).toString() : new Date().toString())
+
+    writeFileSync('.npmrc', `@fortawesome:registry=https://npm.fontawesome.com/\n//npm.fontawesome.com/:_authToken=${getEnvVariable('fontawesome_token')}`)
+
+    // Disable console advertisements during install phase
+    setEnvVariable('ADBLOCK', true);
+
+    const buildScript = getEnvVariable('build_script') || 'yarn build';
+    run(buildScript);
+  } catch (error) {
+    console.log(error)
   }
-  const platform = getPlatform();
-
-  // Make sure `package.json` file exists
-  verifyPackageJson();
-
-  // Copy 'github_token' input variable to 'GH_TOKEN' env variable (required by `electron-builder`)
-  setEnvVariable('GH_TOKEN', getEnvVariable('github_token', true));
-
-  // Require code signing certificate and password if building for macOS. Export them to environment
-  // variables (required by `electron-builder`)
-  if (platform === 'mac') {
-    setEnvVariable('CSC_LINK', getEnvVariable('mac_certs'));
-    setEnvVariable('CSC_KEY_PASSWORD', getEnvVariable('mac_certs_password'));
-  } else if (platform === 'windows') {
-    setEnvVariable('CSC_LINK', getEnvVariable('windows_certs'));
-    setEnvVariable('CSC_KEY_PASSWORD', getEnvVariable('windows_certs_password'));
-  }
-
-  setEnvVariable('BUILD', getEnvVariable('build'))
-  setEnvVariable('PROJECT', getEnvVariable('project'))
-  setEnvVariable('PROJECT_NAME', getEnvVariable('project_name'))
-  setEnvVariable('CI', getEnvVariable('ci'))
-  setEnvVariable('GENERATE_SOURCEMAP', getEnvVariable('generate_sourcemap') || false)
-  setEnvVariable('ENABLE_AUTH', getEnvVariable('enable_auth') || false)
-  setEnvVariable('REACT_APP_MIXPANEL_TOKEN', getEnvVariable('react_app_mixpanel_token'))
-  setEnvVariable('PREMIUM_EDITOR', getEnvVariable('premium_editor'))
-  setEnvVariable('BUILD_ID', getEnvVariable('build_id'))
-  setEnvVariable('COMMIT_ID', getEnvVariable('commit_id').toString())
-  setEnvVariable('BUILD_TIME', getEnvVariable('build_time') ? new Date(Number(getEnvVariable('build_time'))).toString() : new Date().toString())
-
-  writeFileSync('.npmrc', `@fortawesome:registry=https://npm.fontawesome.com/\n//npm.fontawesome.com/:_authToken=${getEnvVariable('fontawesome_token')}`)
-
-  // Disable console advertisements during install phase
-  setEnvVariable('ADBLOCK', true);
-
-  const buildScript = getEnvVariable('build_script') || 'yarn build';
-  run(buildScript);
 };
 
 runAction();
